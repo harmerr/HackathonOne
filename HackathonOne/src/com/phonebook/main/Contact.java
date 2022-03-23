666b3755e185ca079f313ed78153d8775db9902f
DavidToribio1 <72708052+DavidToribio1@users.noreply.github.com>
2022-03-23 11:07:13 -0700

    Added Classes

--- HackathonOne/src/com/phonebook/main/Contact.java
+++ HackathonOne/src/com/phonebook/main/Contact.java
@@ -1,51 +1,50 @@
+//David Toribio
+
 package com.phonebook.main;
-
-
 
 import java.time.LocalDateTime;
 
-
-
 public class Contact {
 
-private String name;
-private String phonebook;
-private LocalDateTime createdat;
+	protected String name;
+	protected String phonebook;
+	protected LocalDateTime createdat;
 
+	public Contact(String name, String phonebook) {
+		super();
+		this.name = name;
+		this.phonebook = phonebook;
+		this.createdat = LocalDateTime.now();
+	}
 
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
 
-public Contact(String name, String phonebook, LocalDateTime createdat) {
-super();
-this.name = name;
-this.phonebook = phonebook;
-this.createdat = createdat;
-}
-public String getName() {
-return name;
-}
-public void setName(String name) {
-this.name = name;
-}
-public String getPhonebook() {
-return phonebook;
-}
-public void setPhonebook(String phonebook) {
-this.phonebook = phonebook;
-}
-public LocalDateTime getCreatedat() {
-return createdat;
-}
-public void setCreatedat(LocalDateTime createdat) {
-this.createdat = createdat;
-}
+	public String getPhonebook() {
+		return phonebook;
+	}
 
-public boolean verifier() {
-return false;
+	public void setPhonebook(String phonebook) {
+		this.phonebook = phonebook;
+	}
 
-}
+	public LocalDateTime getCreatedat() {
+		return createdat;
+	}
 
-//hello
+	public void setCreatedat(LocalDateTime createdat) {
+		this.createdat = createdat;
+	}
+
+	public boolean verifier() {
+		return false;
+
+	}
 
 
 
