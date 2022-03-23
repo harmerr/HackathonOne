ed9881dd64c7184b6d9208873a9c4a4d64f386d9
harmerr <101577717+harmerr@users.noreply.github.com>
2022-03-23 13:58:07 -0400

    Format

--- HackathonOne/src/com/phonebook/main/Person.java
+++ HackathonOne/src/com/phonebook/main/Person.java
@@ -1,21 +1,18 @@
 package com.phonebook.main;
 
-
-
 import java.sql.Date;
 import java.time.LocalDateTime;
 
-
-
 public class Person extends Contact {
 
-private String email;
-private Date dob;
-public Person(String name, String phonebook, LocalDateTime createdat, String email, Date dob) {
-super(name, phonebook, createdat);
-this.email = email;
-this.dob = dob;
-}
+	private String email;
+	private Date dob;
+
+	public Person(String name, String phonebook, LocalDateTime createdat, String email, Date dob) {
+		super(name, phonebook, createdat);
+		this.email = email;
+		this.dob = dob;
+	}
 
 @Override
 public String toString() {
