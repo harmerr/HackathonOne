d903785db4678adf9bafc5da0cd73e6e0768d304
TangoTuesday <99271990+TangoTuesday@users.noreply.github.com>
2022-03-23 16:00:46 -0500

    I have created a basic structure for the hashMap to be implemented as seen fit through out our do-while logic loop.

--- HackathonOne/src/com/phonebook/main/Main.java
+++ HackathonOne/src/com/phonebook/main/Main.java
@@ -17,6 +17,60 @@ public class Main {
 		
 		Scanner input = new Scanner(System.in);
 		
+		
+	/*	HashMap<String, String> organizations = new  HashMap <String, String> (); 
+		HashMap<String, String> persons = new HashMap <String, String> (); 
+		organizations.put(null, null);
+		organizations.remove(null, null); 
+		organizations.get(null, null); 
+		persons.put(null, null); 
+		persons.remove(null, null); 
+		persons.get(null, null); 
+		persons.put(null, null); 
+		
+		
+		//this prints keys using a for loop
+		 
+		for(String a : organizations.keySet()) {
+			System.out.println(a);
+			
+			}
+		
+		for(String b : persons.keySet()) {
+			System.out.println(b);
+			
+			}
+			
+		//this prints values using a for loop
+		 
+		 for(String c : organizations.values()) {
+		 	System.out.println(c); 
+		 	
+		 	}
+			
+		for(String d : persons.valuse()) {	
+			System.out.println(d);
+			
+			}
+			
+		// this prints the keys and values using a for loop
+		 
+		 for(String e : organizations.keySet()) {
+		 	System.out.println ("key: " + e + "value: " + organizations.get(e));
+		 	
+		 	}
+			
+			for(String f : persons.keySet()) {
+		 	System.out.println ("key: " + f + "value: " + persons.get(f));
+		 	
+		 	}
+		
+		this is the basic layout for the hashMap
+		until we can firgure out how to implement these commands into the do-while loop
+		
+		*/
+		
+		
 		//System.out.println("Import Person Or Organization");
 	
 		
