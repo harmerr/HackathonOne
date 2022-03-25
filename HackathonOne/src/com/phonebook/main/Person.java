4afa450856ef719546de665822ba8b0df23defcc
harmerr <101577717+harmerr@users.noreply.github.com>
2022-03-25 10:28:33 -0400

    Cleaned up code and added comments

--- HackathonOne/src/com/phonebook/main/Person.java
+++ HackathonOne/src/com/phonebook/main/Person.java
@@ -5,15 +5,18 @@ import java.io.Serializable;
 
 public class Person extends Contact implements Serializable {
 
+	//Private attributes for the Person child class
 	private String email;
 	private String dob;
 
+	//Constructor for the Person class
 	public Person(String name, String phoneNumber, LocalDateTime createdAt, String email, String dob) {
 		super(name, phoneNumber, createdAt);
 		this.email = email;
 		this.dob = dob;
 	}
 
+	//toString() to return Person object attributes to print
 	@Override
 	public String toString() {
 		return "Person [Name: " + name + ", date of birth: " + dob + ", eMail: " + email + ", phone number=" + phoneNumber
